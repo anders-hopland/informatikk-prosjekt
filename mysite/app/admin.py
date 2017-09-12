27669a93703a5bfc1d097b57fc65e27e78561d68
Andreas Ore Larssen <andreasol@me.com>
2017-09-12 15:34:16 +0200

    Database uplink

--- mysite/app/admin.py
+++ mysite/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app.models import extend_user, rigging, Konsert, Artist, Tilbud
 
 # Register your models here.
+admin.site.register(extend_user)
+admin.site.register(rigging)
+admin.site.register(Konsert)
+admin.site.register(Tilbud)
+admin.site.register(Artist)
