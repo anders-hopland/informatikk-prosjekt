c6032990c2f3777a5178c371286aa9fc6c12d99f
Thomas André Skaaheim <tull-0@hotmail.com>
2017-10-04 12:42:30 +0200

    Adds href to scener i scener.html

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -119,3 +119,10 @@ def bookingsjef(request):
     else:
         return render(request, 'registration/login.html', {})
 
+def scener(request):
+    user = request.user
+    if not request.user.is_authenticated():
+        return render(request, 'registration/login.html', {})
+
+    rolle = user.profile.role
+    return render(request, 'app/scener.html', {'rolle': rolle})
