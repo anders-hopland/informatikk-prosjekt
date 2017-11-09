93b7a076ff471facb7ffa84a9dd3e36f5c643e04
simola <31502545+simola@users.noreply.github.com>
2017-11-09 09:41:16 +0100

    Removes one round() operation on pris

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -544,7 +544,7 @@ def generer_billettpris(request):
             if tilskuertall_snitt <= 0:
                 pris = 0
             else:
-                pris = math.ceil(((totale_kostnader+(tilskuertall_total*300)) / tilskuertall_snitt) * 1.2)
+                pris = ((totale_kostnader+(tilskuertall_total*300)) / tilskuertall_snitt) * 1.2
                 pris = math.ceil(pris / 10)
                 pris = pris*10
 
